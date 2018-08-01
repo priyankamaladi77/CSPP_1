157d42483f1d43acf97ec87b2609328c0c975ea3
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-01 13:54:35 +0530

    act11

---
+++ CSPP_1/day 3/activity11.py
@@ -0,0 +1,10 @@
+varA = 5
+varB = 5
+if type(varA) == str or type(varB) == str:
+	print(" string involved")
+elif varA>varB:
+	print("bigger")
+elif varA == varB:
+	print("equal")
+else:
+	print("smaller")

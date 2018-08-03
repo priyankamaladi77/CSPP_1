276e72439536b44eab371404b465e32d7a2c704b
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-03 20:01:27 +0530

    " -> 0 of 3 passed. pylint: -18.00/10 "

--- CSPP_1/day 5/p4/square_root_newtonrapson.py
+++ CSPP_1/day 5/p4/square_root_newtonrapson.py
@@ -1,9 +1,18 @@
-# Write a python program to find the square root of the number 
+'''newton'''# Write a python program to find the square root of the number
 # using Newton-Rapson method
 
 def main():
-	s = raw_input()
-	#your code here
+    '''newton'''
+    #s = raw_input()
+    #your code here
+    val = int(input())
+    epsilon = 0.01
+    ans = val/2.0
+    count = 0
+    while abs(ans*ans - val) >= epsilon:
+        count += 1
+        ans = ans - (((ans**2) - val)/ (2*ans))
+    print(str(ans))
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()

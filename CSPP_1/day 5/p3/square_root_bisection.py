1ebe99a9ec3757438fbb0a4ed291a2a044d7091f
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-03 19:08:40 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- CSPP_1/day 5/p3/square_root_bisection.py
+++ CSPP_1/day 5/p3/square_root_bisection.py
@@ -1,4 +1,4 @@
-# Write a python program to find the square root of the given number
+'''bisection'''# Write a python program to find the square root of the given number
 # using approximation method
 
 # testcase 1
@@ -10,12 +10,23 @@
 # output: 6.999999999999991
 
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''bisection'''
+    #s = raw_input()
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    #step = 0.1
+    # your code starts here
+    val = int(input())
+    low = 0.0
+    high = val
+    ans = (high + low)/2.0
+    while abs(ans**2 - val) >= epsilon:
+        if ans**2 < val:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/2.0
+    print(ans)
+if __name__ == "__main__":
+    main()

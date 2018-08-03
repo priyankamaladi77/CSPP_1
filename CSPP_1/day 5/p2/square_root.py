c0dddc6c96e3381867d8f6c65b03cf1d3163b8b3
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-03 19:31:47 +0530

    " -> 0 of 3 passed. pylint: 10.00/10 "

--- CSPP_1/day 5/p2/square_root.py
+++ CSPP_1/day 5/p2/square_root.py
@@ -1,4 +1,4 @@
-# Write a python program to find the square root of the given number
+'''square'''# Write a python program to find the square root of the given number
 # using approximation method
 
 # testcase 1
@@ -10,12 +10,23 @@
 # output: 6.999999999999991
 
 def main():
-	s = raw_input()
-	# epsilon and step are initialized
-	# don't change these values
-	epsilon = 0.01
-	step = 0.1
-	# your code starts here
-
-if __name__== "__main__":
-	main()
+    '''square'''
+    #s = raw_input()
+    # epsilon and step are initialized
+    # don't change these values
+    epsilon = 0.01
+    #step = 0.1
+    # your code starts here
+    val = int(input())
+    ans = 0.0
+    inc = 0.1
+    count = 0
+    while abs(ans**2 - val) >= epsilon:
+        ans += inc
+        count += 1
+    if abs(ans**2 - val) >= epsilon:
+        print("failed on square root of", val)
+    else:
+        print(ans, 'is close square root of', val)
+if __name__ == "__main__":
+    main()

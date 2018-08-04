a5b4d9ce244cf405d33649c7d5dc3834861fd8fb
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-04 17:06:23 +0530

    CSPP1

--- CSPP_1/day 5/GuessMyNumber/guess_my_number.py
+++ CSPP_1/day 5/GuessMyNumber/guess_my_number.py
@@ -1,8 +1,23 @@
-#Guess My Number Exercise
+'''Guess My Number Exercise'''
 
 def main():
-	s = raw_input()
-	#your code here
-
-if __name__== "__main__":
-	main()
+    ''' guessing '''
+    #s = raw_input()
+    #your code here
+    mid = 50
+    high = 100
+    low = 0
+    inp = 'l'
+    while inp != 'c':
+        print(mid)
+        inp = input("Enter 'h' if guess is too high,'l' if its too low.\
+            'c' to indicate I guessed correctly")
+        if inp == 'h':
+            high = mid
+            mid = (high + low) // 2
+        elif inp == 'l':
+            low = mid
+            mid = (high + low) // 2
+    print('your guess number is :', mid)
+if __name__ == "__main__":
+    main()

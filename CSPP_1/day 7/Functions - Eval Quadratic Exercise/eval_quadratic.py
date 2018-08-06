5ffbbecc4c522dc8a6e2ecebb0b37d8079cfc54e
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-06 14:57:07 +0530

    quad

--- CSPP_1/day 7/Functions - Eval Quadratic Exercise/eval_quadratic.py
+++ CSPP_1/day 7/Functions - Eval Quadratic Exercise/eval_quadratic.py
@@ -1,26 +1,30 @@
+'''quadratic'''
 # Exercise: eval quadratic
 
-# Write a Python function, evalQuadratic(a, b, c, x), that returns the value of the quadratic a . x 2 + b . x + c
+# Write a Python function, evalQuadratic(a, b, c, x),
+# that returns the value of the quadratic a . x 2 + b . x + c
 
 # This function takes in four numbers and returns a single number.
 
 
-def evalQuadratic(a, b, c, x):
-    return a*x**2+b*x+c
+
+def evalQuadratic(va1, va2, va3, res):
+    '''quadratic'''
+    return va1*res**2+va2*res+va3
 
 def main():
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	# print(data)
-	for x in range(len(data)):
-		temp = str(data[x]).split('.')
-		if(temp[1] == '0'):
-			data[x] = int(float(str(data[x])))
-		else:
-			data[x] = data[x]
-	print(evalQuadratic(data[0],data[1],data[2],data[3]))
+    '''quadratic'''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    # print(data)
+    for res in enumerate(data):
+        temp = str(data[res]).split('.')
+        if temp[1] == '0':
+            data[res] = int(float(str(data[res])))
+        else:
+            data[res] = data[res]
+    print(evalQuadratic(data[0], data[1], data[2], data[3]))
 
-if __name__== "__main__":
-	main()
-
+if __name__ == "__main__":
+    main()

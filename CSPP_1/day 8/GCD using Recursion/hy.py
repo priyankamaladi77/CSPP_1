7c1cc50d9d90d51f4641848ce9bc6dd51cc58a29
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-07 18:09:44 +0530

    " -> 9 of 10 passed. pylint: 10.00/10 "

---
+++ CSPP_1/day 8/GCD using Recursion/hy.py
@@ -0,0 +1,8 @@
+aa = "AA"
+aaa = "AAA"
+c = "C"
+d = "D" 
+
+from batteries import *
+aa = "aa"
+print(aa, aaa, c, d)
---
+++ CSPP_1/day 8/p1/assignment1.py
@@ -0,0 +1,29 @@
+'''factorial'''
+# Exercise: Assignment-1
+# Write a Python function, factorial(n),
+# that takes in one number and returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+
+def factorial(ans):
+    '''
+    n is positive Integer
+
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+    if ans == 1:
+        return ans
+
+    return ans * factorial(ans-1)
+
+
+
+def main():
+    '''factorial'''
+    val = input()
+    print(factorial(int(val)))
+
+if __name__ == "__main__":
+    main()

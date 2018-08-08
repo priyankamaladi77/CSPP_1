a36226f0a1a7414cce6895c2c233915328e44976
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-08 18:45:45 +0530

    " -> 9 of 9 passed. pylint: 9.52/10 "

--- CSPP_1/day 9/p1/assignment1.py
+++ CSPP_1/day 9/p1/assignment1.py
@@ -18,7 +18,7 @@ def is_word_guessed(secret_word, letters_guessed):
     # FILL IN YOUR CODE HERE...
     res = 0
     for char in range(len(secret_word)):
-        if char in letters_guessed:
+        if secret_word[char] in letters_guessed:
             res += 1
     if res == len(secret_word):
         return True

eb792749ade209965ae090f5936ea2e0e4b6b9d2
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-23 22:16:41 +0530

    " -> 7 of 8 passed. pylint: 9.20/10 "

--- CSPP_1/module 20/CodeCampMatrixOperations/matrix_operations.py
+++ CSPP_1/module 20/CodeCampMatrixOperations/matrix_operations.py
@@ -32,7 +32,7 @@ def add_matrix(mat1, mat2):
     '''
     res = copy.deepcopy(mat1)
     if len(mat1) == len(mat2):
-        for i in enumerate(mat1):
+        for i in range(len(mat1)):
             for j in range(len(mat1[i])):
                 temp = int(res[i][j])
                 temp += int(mat2[i][j])
@@ -72,6 +72,7 @@ def read_matrix(size):
         return None
     else:
         return matrix
+
 def main():
     ''' to read the functions'''
     # read matrix 1

43093c384f6e4939cbebd7615ff3b9125547c543
priyankamaladi77 <svanmp@msitprogram.net>
2018-08-24 15:00:02 +0530

    " -> 14 of 14 passed. pylint: 9.71/10 "

--- CSPP_1/module 21/CodeCampTicTacToe/tic_tac_toe.py
+++ CSPP_1/module 21/CodeCampTicTacToe/tic_tac_toe.py
@@ -1,73 +1,77 @@
+'''logic for tic_tac_toe'''
 def looping(seti):
-	if 'x' in seti:
-		return 'x'
-	return 'o'
+    ''' looping '''
+    if 'x' in seti:
+        return 'x'
+    return 'o'
 
 def game_play(game):
+    ''' game play function'''
+    set_1 = set()
+    set_2 = set()
+    set_3 = set()
+    set_4 = set()
+    set_5 = set()
+    length_1 = len(game)
+    for i in range(length_1):
+        for j in range(len(game[i])):
+            if i == j:
+                set_1.add(game[i][j])
+            if i + j == (len(game)-1):
+                set_2.add(game[i][j])
+            set1 = set(game[i])
+            if len(set1) == 1:
+                if 'x' in set1:
+                    return 'x'
+                return 'o'
+            if j == 0:
+                set_3.add(game[i][j])
+            if j == 1:
+                set_4.add(game[i][j])
+            if j == 2:
+                set_5.add(game[i][j])
+    if len(set_1) == 1:
+        return looping(set_1)
+    if len(set_2) == 1:
+        return looping(set_2)
+    if len(set_3) == 1:
+        return looping(set_3)
+    if len(set_4) == 1:
+        return looping(set_4)
+    if len(set_5) == 1:
+        return looping(set_5)
+    return "draw"
 
-	set1 = set()
-	set2 = set()
-	set3 = set()
-	set4 = set()
-	set5 = set()
-	length = len(game)
-	for i in range(length):
-		for j in range(len(game[i])):
-			if i == j:
-				set1.add(game[i][j])
-			if i + j == (len(game)-1):
-				set2.add(game[i][j])
-			sett = set(game[i])
-			if len(sett) == 1:
-				if 'x' in sett:
-					return 'x'
-				return 'o'
-			if j == 0:
-				set3.add(game[i][j])
-			if j == 1:
-				set4.add(game[i][j])
-			if j == 2:
-				set5.add(game[i][j])
-	if len(set1) == 1:
-		return looping(set1)
-	if len(set2) == 1:
-		return looping(set2)
-	if len(set3) == 1:
-		return looping(set3)
-	if len(set4) == 1:
-		return looping(set4)
-	if len(set5) == 1:
-		return looping(set5)
-	return "draw"
-
-def validity_check(game):
-	x_element = 'x'
-	o_element = 'o'
-	xcount = 0
-	ocount = 0
-	countt = 0
-	for index in game:
-		if len(index) != 3:
-			return "invalid game"
-		if x_element in index:
-			xcount += index.count(x_element)
-		if o_element in index:
-			ocount += index.count(o_element)
-		if '.' in index:
-			countt += index.count('.')
-	if xcount+ocount+countt != 9:
-		return "invalid input"
-	if abs(xcount - ocount) != 1:
-		return "invalid game" 
-	return game_play(game)
+def check_for_validity(game):
+    '''validation'''
+    x_element = 'x'
+    o_element = 'o'
+    x_count = 0
+    o_count = 0
+    count1 = 0
+    for index in game:
+        if len(index) != 3:
+            return "invalid game"
+        if x_element in index:
+            x_count += index.count(x_element)
+        if o_element in index:
+            o_count += index.count(o_element)
+        if '.' in index:
+            count1 += index.count('.')
+    if x_count+o_count+count1 != 9:
+        return "invalid input"
+    if abs(x_count - o_count) != 1:
+        return "invalid game"
+    return game_play(game)
 
 def main():
-	row = []
-	for _ in range(3):
-		column = input()
-		column = list(map(str, column.split(' ')))
-		row.append(column)
-	row = validity_check(row)
-	print(row)
+    '''main function'''
+    rows = []
+    for _ in range(3):
+        columns = input()
+        columns = list(map(str, columns.split(' ')))
+        rows.append(columns)
+    rows = check_for_validity(rows)
+    print(rows)
 if __name__ == '__main__':
-	main()
+    main()
